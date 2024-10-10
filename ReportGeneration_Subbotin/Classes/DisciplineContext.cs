062e876d496ceec4712042b30550a07a1753d224
kishlakhub <gsubbotin.2019@gmail.com>
2024-10-10 16:54:27 +0500

    Разработанные контексты данных для работы с БД. Subbotin

---
+++ ReportGeneration_Subbotin/Classes/DisciplineContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using ReportGeneration_Subbotin.Classes.Common;
+using ReportGeneration_Subbotin.Models;
+
+
+namespace ReportGeneration_Subbotin.Classes
+{
+    public class DisciplineContext : Discipline
+    {
+        public DisciplineContext(int Id, string Name, int IdGroup) : base(Id, Name, IdGroup) { }
+        public static List<DisciplineContext> AllDisciplines()
+        {
+            List<DisciplineContext> allDisciplines = new List<DisciplineContext>();
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader BDDisciplines = Connection.Query("SELECT * FROM `Discipline` ORDER BY `Name`", connection);
+            while (BDDisciplines.Read())
+            {
+                allDisciplines.Add(new DisciplineContext(
+                    BDDisciplines.GetInt32(0),
+                    BDDisciplines.GetString(1),
+                    BDDisciplines.GetInt32(2)));
+            }
+            Connection.CloseConnection(connection);
+            return allDisciplines;
+        }
+    }
+}
